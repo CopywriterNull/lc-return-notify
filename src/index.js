df6daac728b60521bd58e51a13ed9ce24f4789f8
Lenny Huynh <lennyhuynh526@gmail.com>
2023-06-10 21:28:12 -0700

    lfg

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
-import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 console.log(__dirname);
+
 // get environment variables
 const port = process.env.PORT || 3000;
 const nodeEnv = process.env.NODE_ENV;
