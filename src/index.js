3d538399c46d37be82e87eeae138f290d0f9372b
Lenny Huynh <lennyhuynh526@gmail.com>
2023-06-10 20:50:29 -0700

    .

--- src/index.js
+++ src/index.js
@@ -9,7 +9,16 @@ const version = 4;
 
 // setup express
 const app = express();
+const path = require('path');
 app.use(express.json());
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route handler for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // save albums in memory
 let albums = [{
